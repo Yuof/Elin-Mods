dd6da335495818a8f888890a6f726019eb5701f6
Roman <roman@displayer.info>
2024-11-20 03:26:33 +0100

    add loops for skillhelper

---
+++ Elin_SkillHelper/Extensions.cs
@@ -0,0 +1,25 @@
+public static class Extensions
+{
+    public static Point GetDestinationPoint(this AIAct act)
+    {
+        if (act is TaskPoint taskPoint)
+        {
+            return taskPoint.pos;
+        }
+        else
+        {
+            return act.GetDestination();
+        }
+    }
+
+    public static int RealDistance(this Point a, Point b)
+    {
+        var path = new PathProgress();
+        path.RequestPathImmediate(a, b, 0, false);
+        if (path.nodes.Count == 0)
+        {
+            return int.MaxValue;
+        }
+        return path.nodes.Count;
+    }
+}
--- Elin_SkillHelper/Plugin.cs
+++ Elin_SkillHelper/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using Elin_SkillHelper;
 using HarmonyLib;
 using System;
@@ -12,6 +13,8 @@ namespace ExampleMod;
 [BepInPlugin("yuof.elin.skillhelper.mod", "Elin SkillHelper", "1.0.0.0")]
 public class Plugin : BaseUnityPlugin
 {
+    public SkillHelperMode mode = SkillHelperMode.None;
+
     private void Start()
     {
         var harmony = new Harmony("yuof.elin.skillhelper.mod");
@@ -22,29 +25,134 @@ public class Plugin : BaseUnityPlugin
     public void Update()
     {
         if (EInput.isInputFieldActive) return;
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            this.Logger.LogInfo("I key pressed");
+            this.mode = SkillHelperMode.None;
+        }
+        if (!EClass.core.IsGameStarted
+        || ELayer.pc == null
+        || ELayer.pc.isDead
+        || !ELayer.pc.ai.IsIdle
+        || WidgetCurrentTool.dirty)
+        {
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.O))
         {
             this.Logger.LogInfo("O key pressed");
             //TSUI.LoadCustomUI<SkillHelperWindow, SkillHelperWindow.Args>(new SkillHelperWindow.Args());
-            var shearables = this.FindShearables();
-            if (!shearables.Any())
-            {
-                ELayer.pc.Say("invalidAction");
-                return;
-            }
-            shearables = shearables.OrderBy(chara => ELayer.pc.Dist(chara)).ToList();
-            this.WieldShears();
-            var chara = shearables.First();
-            ELayer.pc.SetAIImmediate(new AI_Shear { target = chara });
+            this.mode = SkillHelperMode.Shear;
         }
         if (Input.GetKeyDown(KeyCode.P))
         {
             this.Logger.LogInfo("P key pressed");
-            var bestPoint = this.FindBestLocationForPerformance();
-            var ai = new AI_Goto(bestPoint, 0);
-            this.Logger.LogInfo("bestPoint: " + bestPoint);
-            ELayer.pc.SetAIImmediate(ai);
+            this.mode = SkillHelperMode.Performance;
         }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            this.Logger.LogInfo("K key pressed");
+            this.mode = SkillHelperMode.Water;
+        }
+
+        switch (this.mode)
+        {
+            case SkillHelperMode.Shear:
+                this.HandleShearing();
+                break;
+            case SkillHelperMode.Water:
+                this.HandleWatering();
+                break;
+            case SkillHelperMode.Performance:
+                this.HandlePerformance();
+                break;
+        }
+    }
+
+    private void HandleWatering()
+    {
+        // if can is empty, refill it
+        var acts = new List<AIAct>();
+        var waterCan = this.WieldWaterCan();
+        if (waterCan != null) {
+                Logger.LogInfo("WaterCan charges: " + waterCan.owner.c_charges);
+                if (waterCan.owner.c_charges == 0)
+                {
+                    ELayer._map.bounds.ForeachPoint(point =>
+                    {
+                        if (ActDrawWater.HasWaterSource(point))
+                        {
+                            Logger.LogInfo("Found water source at: " + point);
+                            if (point.RealDistance(ELayer.pc.pos) > 1)
+                            {
+                                Logger.LogInfo("Going to water source");
+                                acts.Add(new AI_Goto(point, 1));
+                                return;
+                            }
+                            var act = new ActDrawWater() { waterCan = waterCan };
+                            act.Perform();
+                            acts = this.FindWaterAbles();
+                        }
+                    });
+                }
+                else
+                {
+                    acts = this.FindWaterAbles();
+                }
+        }
+                
+        if (!acts.Any())
+        {
+            ELayer.pc.Say("invalidAction");
+            this.mode = SkillHelperMode.None;
+            return;
+        }
+        var act = acts.OrderBy(act => act.GetDestinationPoint().RealDistance(ELayer.pc.pos)).First();
+        ELayer.pc.SetAIImmediate(act);
+    }
+
+    private void HandlePerformance()
+    {
+        var bestPoint = this.FindBestLocationForPerformance();
+        Logger.LogInfo("bestPoint: " + bestPoint);
+        Logger.LogInfo("pc.pos: " + ELayer.pc.pos);
+        if (bestPoint != null && ELayer.pc.pos.Distance(bestPoint) == 0)
+        {
+            ELayer.pc.TalkRaw("Starting music.");
+            // equip instrument
+            var instrument = ELayer.pc.things.Where(thing => thing.trait as TraitToolMusic != null).FirstOrDefault();
+            if (instrument == null)
+            {
+                ELayer.pc.Say("invalidAction");
+                this.mode = SkillHelperMode.None;
+                return;
+            }
+            else
+            {
+                ELayer.player.EquipTool(instrument);
+                ELayer.pc.SetAIImmediate(new AI_PlayMusic() { tool = instrument });
+                this.mode = SkillHelperMode.None;
+                return;
+            }
+        }
+        var ai = new AI_Goto(bestPoint, 0);
+        this.Logger.LogInfo("bestPoint: " + bestPoint);
+        ELayer.pc.SetAIImmediate(ai);
+    }
+
+    private void HandleShearing()
+    {
+        var shearables = this.FindShearables();
+        if (!shearables.Any())
+        {
+            ELayer.pc.Say("invalidAction");
+            this.mode = SkillHelperMode.None;
+            return;
+        }
+        var chara = shearables.OrderBy(chara => ELayer.pc.pos.RealDistance(chara.pos)).First();
+        this.WieldShears();
+        ELayer.pc.SetAIImmediate(new AI_Shear { target = chara });
     }
 
     private List<Chara> FindShearables()
@@ -67,12 +175,40 @@ public class Plugin : BaseUnityPlugin
         }
     }
 
+    private List<AIAct> FindWaterAbles()
+    {
+        var map = ELayer._map;
+        var list = new List<AIAct>();
+        map.bounds.ForeachPoint(point =>
+        {
+
+            if (TaskWater.ShouldWater(point))
+            {
+                var act = new TaskWater() { dest = point.Copy(), owner = ELayer.pc };
+                list.Add(act);
+            }
+        });
+        return list;
+    }
+
+    private TraitToolWaterCan? WieldWaterCan()
+    {
+        var pc = ELayer.pc;
+        //find water can
+        var waterCan = pc.things.Where(thing => thing.trait as TraitToolWaterCan != null).FirstOrDefault();
+        if (waterCan != null)
+        {
+            ELayer.player.EquipTool(waterCan);
+        }
+        return waterCan?.trait as TraitToolWaterCan;
+    }
+
     private Point FindBestLocationForPerformance()
     {
         var map = ELayer._map;
         var bestPoint = map.GetCenterPos();
         double bestScore = 0;
-        map.ForeachPoint(point =>
+        map.bounds.ForeachPoint(point =>
         {
             var charas = point.ListWitnesses(ELayer.pc, 4, WitnessType.music);
             charas = charas
@@ -93,4 +229,12 @@ public class Plugin : BaseUnityPlugin
     }
 
 
+
+    public enum SkillHelperMode
+    {
+        None,
+        Shear,
+        Water,
+        Performance,
+    }
 }
